38783ca376eb0efc10000722e37a9fb1a0f4421e
francisco2016 <otrayama@gmail.com>
2016-01-24 05:44:29 +0100

    0049 multiplesOfFive() imprime por pantalla los multiplos de 5

---
+++ Calculadora.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Calculadora here.
+ * 
+ * @author (Francisco) 
+ * @version (ejercicio de programación 0049)
+ */
+public class Calculadora
+{
+  /**
+  *  imprima todos los múltiplos de 5 entre 10 y 95 (ambos extremos no incluidos).
+  */
+  public void multiplesOfFive(){
+    int cont = 15;
+    while(cont <= 90){
+        System.out.println(cont);
+                cont = cont +5;
+    }
+    
+    
+  }
+}
