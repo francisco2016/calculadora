303cd936aa355d569673bd4959ab65c685487529
francisco2016 <otrayama@gmail.com>
2016-01-24 06:02:52 +0100

    0049 sumValues() suma los nº comprendido entre 0 y 10

--- Calculadora.java
+++ Calculadora.java
@@ -16,7 +16,41 @@ public class Calculadora
         System.out.println(cont);
                 cont = cont +5;
     }
-    
-    
   }
+  
+  /**
+   * que saque por pantalla la suma de los números comprendidos entre el 0 y el 10 (ambos incluidos).
+   */
+  public void sumValues(){
+      int cont = 0;
+      int suma = 0;
+      while(cont <= 11){
+          suma = cont + suma;
+          cont ++;
+      }
+      System.out.println("");
+      System.out.println("La suma es: " +suma);
+  }
+  
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
